1a9cf172db3feb82c09b10968e5668639ef2bc3c
Bhavishya <121146811+bhavishya2324@users.noreply.github.com>
2024-07-28 10:04:01 +0530

    Update index.jsx

--- app/components/homepage/experience/index.jsx
+++ app/components/homepage/experience/index.jsx
@@ -5,13 +5,16 @@ import Image from "next/image";
 import { BsPersonWorkspace } from "react-icons/bs";
 import AnimationLottie from "../../helper/animation-lottie";
 import GlowCard from "../../helper/glow-card";
-import experience from '/public/lottie/code.json';
+import experience from "/public/lottie/code.json";
 import Link from "next/link";
 import { FaArrowRight } from "react-icons/fa";
 
 function Experience() {
   return (
-    <div id="experience" className="relative z-50 border-t my-12 lg:my-24 border-[#25213b]">
+    <div
+      id="experience"
+      className="relative z-50 border-t my-12 lg:my-24 border-[#25213b]"
+    >
       <Image
         src="/section.svg"
         alt="Hero"
@@ -31,67 +34,64 @@ function Experience() {
       </div>
 
       <div className="py-8">
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16">
+        <div className="grid grid-cols-1 lg:grid-cols gap-8 lg:gap-16">
           <div className="flex justify-center items-start">
-            <div className="w-full h-full">
+            <div className="lg:w-2/4 lg:h-2/4">
               <AnimationLottie animationPath={experience} />
             </div>
           </div>
 
           <div>
             <div className="flex flex-col gap-6">
-              {
-                experiences.map(experience => (
-                  <GlowCard key={experience.id} identifier={`experience-${experience.id}`}>
-                    <div className="p-2 relative">
-                      <Image
-                        src="/blur-23.svg"
-                        alt="Hero"
-                        width={1080}
-                        height={200}
-                        className="absolute bottom-0 opacity-80"
-                      />
-                      <div className="flex justify-center">
-                        <p className="text-xs sm:text-sm text-[#16f2b3]">
-                          {experience.duration}
+              {experiences.map((experience) => (
+                <GlowCard
+                  key={experience.id}
+                  identifier={`experience-${experience.id}`}
+                >
+                  <div className="p-2 relative">
+                    <Image
+                      src="/blur-23.svg"
+                      alt="Hero"
+                      width={1080}
+                      height={200}
+                      className="absolute bottom-0 opacity-80"
+                    />
+                    <div className="flex justify-center">
+                      <p className="text-xs sm:text-sm text-[#16f2b3]">
+                        {experience.duration}
+                      </p>
+                    </div>
+                    <div className="flex items-center gap-x-8  py-5">
+                      <div className="text-violet-500  transition-all duration-300 hover:scale-125"></div>
+                      <div>
+                        <p className="text-base sm:text-xl mb-2 font-medium uppercase">
+                          {experience.title}
+                        </p>
+                        <p className="text-sm sm:text-base">
+                          {experience.company}
                         </p>
                       </div>
-                      <div className="flex items-center gap-x-8  py-5">
-                        <div className="text-violet-500  transition-all duration-300 hover:scale-125">
-                          
-                        </div>
-                        <div>
-                          <p className="text-base sm:text-xl mb-2 font-medium uppercase">
-                            {experience.title}
-                          </p>
-                          <p className="text-sm sm:text-base">
-                            {experience.company}
-                          </p>
-                        </div>
-                      </div>
                     </div>
-                  </GlowCard>
-                ))
-              }
+                  </div>
+                </GlowCard>
+              ))}
 
-<div className="flex justify-center  mt-5 lg:mt-12">
-        <Link
-          className="flex items-center gap-1 hover:gap-3 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-3 md:px-8 py-3 md:py-4 text-center text-xs md:text-sm font-medium tracking-wider text-white no-underline transition-all duration-200 ease-out hover:text-white hover:no-underline md:font-semibold"
-          role="button"
-          href="/about"
-        >
-          <span className="">About us</span>
-          <FaArrowRight size={20} />
-        </Link>
-      </div>
-
-      
+              <div className="flex justify-center  mt-5 lg:mt-12">
+                <Link
+                  className="flex items-center gap-1 hover:gap-3 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-3 md:px-8 py-3 md:py-4 text-center text-xs md:text-sm font-medium tracking-wider text-white no-underline transition-all duration-200 ease-out hover:text-white hover:no-underline md:font-semibold"
+                  role="button"
+                  href="/about"
+                >
+                  <span className="">About us</span>
+                  <FaArrowRight size={20} />
+                </Link>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default Experience;

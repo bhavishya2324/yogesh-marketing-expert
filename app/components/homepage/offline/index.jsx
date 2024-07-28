c952eab8be6b351a9eb52e9d6e36280c2ad9342d
Bhavishya <121146811+bhavishya2324@users.noreply.github.com>
2024-07-28 10:05:50 +0530

    Update index.jsx

--- app/components/homepage/offline/index.jsx
+++ app/components/homepage/offline/index.jsx
@@ -5,13 +5,16 @@ import { educations } from "@/utils/data/educations";
 import { BsPersonWorkspace } from "react-icons/bs";
 import AnimationLottie from "../../helper/animation-lottie";
 import GlowCard from "../../helper/glow-card";
-import lottieFile from '/public/lottie/study.json';
+import lottieFile from "/public/lottie/study.json";
 import Link from "next/link";
 import { FaArrowRight } from "react-icons/fa";
 
 function Education() {
   return (
-    <div id="education" className="relative z-50 border-t my-12 lg:my-24 border-[#25213b]">
+    <div
+      id="education"
+      className="relative z-50 border-t my-12 lg:my-24 border-[#25213b]"
+    >
       <Image
         src="/section.svg"
         alt="Hero"
@@ -29,83 +32,71 @@ function Education() {
         <div className="flex  items-center">
           <span className="w-24 h-[2px] bg-[#1a1443]"></span>
           <span className="bg-[#1a1443] w-fit text-white p-2 px-5 text-xl rounded-md">
-             Offline
+            Offline
           </span>
           <span className="w-24 h-[2px] bg-[#1a1443]"></span>
         </div>
       </div>
 
       <div className="py-8">
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16">
+        <div className="grid grid-cols-1 lg:grid-cols gap-8 lg:gap-16">
           <div className="flex justify-center items-start">
-            <div className="w-3/4 h-3/4">
+            <div className="lg:w-2/4 lg:h-2/4">
               <AnimationLottie animationPath={lottieFile} />
             </div>
           </div>
 
           <div>
             <div className="flex flex-col gap-6">
-              {
-                educations.map(education => (
-                  <GlowCard key={education.id} identifier={`education-${education.id}`}>
-                    <div className="p-3 relative text-white">
-                      <Image
-                        src="/blur-23.svg"
-                        alt="Hero"
-                        width={1080}
-                        height={200}
-                        className="absolute bottom-0 opacity-80"
-                      />
-                      <div className="flex justify-center">
-                        <p className="text-xs sm:text-sm text-[#16f2b3]">
-                          {education.duration}
+              {educations.map((education) => (
+                <GlowCard
+                  key={education.id}
+                  identifier={`education-${education.id}`}
+                >
+                  <div className="p-3 relative text-white">
+                    <Image
+                      src="/blur-23.svg"
+                      alt="Hero"
+                      width={1080}
+                      height={200}
+                      className="absolute bottom-0 opacity-80"
+                    />
+                    <div className="flex justify-center">
+                      <p className="text-xs sm:text-sm text-[#eedf10]">
+                        {education.duration}
+                      </p>
+                    </div>
+                    <div className="flex items-center gap-x-8  py-5">
+                      <div className="text-violet-500  transition-all duration-300 hover:scale-125"></div>
+                      <div>
+                        <p className="text-base sm:text-xl mb-2 font-medium uppercase">
+                          {education.title}
+                        </p>
+                        <p className="text-sm sm:text-base">
+                          {education.institution}
                         </p>
                       </div>
-                      <div className="flex items-center gap-x-8  py-5">
-                        <div className="text-violet-500  transition-all duration-300 hover:scale-125">
-                         
-                        </div>
-                        <div>
-                          <p className="text-base sm:text-xl mb-2 font-medium uppercase">
-                            {education.title}
-                          </p>
-                          <p className="text-sm sm:text-base">{education.institution}</p>
-                        </div>
-                      </div>
                     </div>
-                  </GlowCard>
+                  </div>
+                </GlowCard>
+              ))}
 
-
-
-
-
-                ))
-
-                
-
-              }
-
-
-              
-      <div className="flex justify-center  mt-5 lg:mt-12">
-        <Link
-          className="flex items-center gap-1 hover:gap-3 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-3 md:px-8 py-3 md:py-4 text-center text-xs md:text-sm font-medium tracking-wider text-white no-underline transition-all duration-200 ease-out hover:text-white hover:no-underline md:font-semibold"
-          role="button"
-          href="/contact"
-        >
-          <span className="">Contact us</span>
-          <FaArrowRight size={20} />
-        </Link>
-      </div>
-
-
-
+              <div className="flex justify-center  mt-5 lg:mt-12">
+                <Link
+                  className="flex items-center gap-1 hover:gap-3 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-3 md:px-8 py-3 md:py-4 text-center text-xs md:text-sm font-medium tracking-wider text-white no-underline transition-all duration-200 ease-out hover:text-white hover:no-underline md:font-semibold"
+                  role="button"
+                  href="/contact"
+                >
+                  <span className="">Contact us</span>
+                  <FaArrowRight size={20} />
+                </Link>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default Education;

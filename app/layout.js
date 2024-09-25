1857f5755ca36f018cf0fd64bbfa4e27bd9d205b
Bhavishya <121146811+bhavishya2324@users.noreply.github.com>
2024-09-25 11:11:07 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -16,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      < className={inter.className}>
+      < body className={inter.className}>
         <ToastContainer />
         <main className="min-h-screen relative mx-auto px-4 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[80rem] text-white">
           <Navbar />
